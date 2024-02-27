5c7497fce8ac8e1a3a2ac276eb22305ea196fce1
nitish099 <nitishk94036@gmail.com>
2024-02-27 10:46:12 +0530

    final changes made

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -41,14 +41,13 @@ class DataTransformation:
                 ]
             )
 
-            cat_pipeline=Pipeline(
-
+            # Categorical Pipeline
+            cat_pipeline = Pipeline(
                 steps=[
-                    ('imputer',SimpleImputer(strategy='most_frequent')),
-                ("one_hot_encoder",OneHotEncoder()),
-                ("scaler",StandardScaler())
+                ('imputer',SimpleImputer(strategy='most_frequent')),
+                ("one_hot_encoder",OneHotEncoder(handle_unknown='ignore')),
+                ('scaler',StandardScaler())
                 ]
-
             )
 
             logging.info(f"Categorical columns: {categorical_columns}")
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -3,7 +3,6 @@ import pandas as pd
 from src.logger import logging
 from src.exception import CustomException
 from src.utils import load_object
-import os
 
 
 class PredictPipeline:
@@ -12,13 +11,13 @@ class PredictPipeline:
 
     def predict(self, features):
         try:
-            preprocessor_path = 'artifacts/preprocessor.pkl'
-            model_path = 'artifacts/model.pkl'
-            preprocessor = load_object(file_path=preprocessor_path)
-            model = load_object(file_path=model_path)
-            data_scaled = preprocessor.transform(features)
-            pred = model.predict(data_scaled)
-            return pred
+            model_path="artifacts\model.pkl"
+            preprocessor_path="artifacts\preprocessor.pkl"
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)  
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
         except Exception as e:
             logging.info('Exception occured in prediction pipeline')
             raise CustomException(e,sys)
