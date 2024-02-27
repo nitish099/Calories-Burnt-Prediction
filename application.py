ec99a06e065400b9dfe8b49a8ddcfc4295872c33
Nitish Sharma <78410580+nitish099@users.noreply.github.com>
2024-02-27 12:41:08 +0530

    Update application.py

--- application.py
+++ application.py
@@ -12,8 +12,6 @@ app=application
 ## Route for a home page
 
 @app.route('/')
-def index():
-    return render_template('index.html') 
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
